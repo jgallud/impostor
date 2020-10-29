92781f6f37a49f00ac9dbbf05e8c47df67c6f88d
jgallud <jgallud@gmail.com>
2020-10-29 17:59:40 +0100

    capa Rest

---
+++ cliente/src/clienteRest.js
@@ -0,0 +1,12 @@
+function ClienteRest(){
+	this.crearPartida=function(nick,num){
+		$.getJSON("/crearPartida/"+nick+"/"+num,function(data){    
+    		console.log(data);
+		});
+	}
+	this.unirAPartida=function(nick,codigo){
+		$.getJSON("/unirAPartida/"+nick+"/"+codigo,function(data){    
+    		console.log(data);
+		});
+	}
+}
--- servidor/modeloSpec.js
+++ servidor/modeloSpec.js
@@ -1,22 +1,31 @@
+var modelo=require("./modelo.js");
+
 describe("El juego del impostor", function() {
   var juego;
   var usr;
 
   beforeEach(function() {
-  	juego=new Juego();
-  	usr=new Usuario("Pepe",juego);
+  	juego=new modelo.Juego();
+  	usr=new modelo.Usuario("Pepe");
   });
 
   it("comprobar valores iniciales del juego", function() {
   	expect(Object.keys(juego.partidas).length).toEqual(0);
   	expect(usr.nick).toEqual("Pepe");
-  	expect(usr.juego).not.toBe(undefined);
+  	//expect(usr.juego).not.toBe(undefined);
   });
+
+  it("comprobar valores de la partida",function(){
+  	var codigo=juego.crearPartida(3,usr);
+  	expect(codigo).toEqual("fallo");
+  	codigo=juego.crearPartida(11,usr);
+  	expect(codigo).toEqual("fallo");
+  })
 
   describe("el usr Pepe crea una partida de 4 jugadores",function(){
 	var codigo;
 	beforeEach(function() {
-	  	codigo=usr.crearPartida(4);
+	  	codigo=juego.crearPartida(4,usr);
 	  });
 
 	it("se comprueba la partida",function(){ 	
@@ -27,6 +36,13 @@ describe("El juego del impostor", function() {
 	 	var num=Object.keys(juego.partidas[codigo].usuarios).length;
 	  	expect(num).toEqual(1);
 	  });
+
+	it("no se puede crear partida si el num no está entre 4 y 10",function(){
+		var codigo=juego.crearPartida(3,usr);
+		expect(codigo).toEqual("fallo");
+		codigo=juego.crearPartida(11,usr);
+		expect(codigo).toEqual("fallo");
+	});
 
 	it("varios usuarios se unen a la partida",function(){
 		juego.unirAPartida(codigo,"ana");
@@ -58,6 +74,31 @@ describe("El juego del impostor", function() {
 		expect(juego.partidas[codigo].fase.nombre).toEqual("completado");		
 		usr.iniciarPartida();
 		expect(juego.partidas[codigo].fase.nombre).toEqual("jugando");
+	});
+	it("abandonar partida",function(){
+		juego.unirAPartida(codigo,"ana");
+	  	var num=Object.keys(juego.partidas[codigo].usuarios).length;
+	  	expect(num).toEqual(2);
+		expect(juego.partidas[codigo].fase.nombre).toEqual("inicial");
+		juego.unirAPartida(codigo,"isa");
+	  	var num=Object.keys(juego.partidas[codigo].usuarios).length;
+	  	expect(num).toEqual(3);
+		expect(juego.partidas[codigo].fase.nombre).toEqual("inicial");	  	
+		juego.unirAPartida(codigo,"tomas");
+	  	var num=Object.keys(juego.partidas[codigo].usuarios).length;
+	  	expect(num).toEqual(4);
+		expect(juego.partidas[codigo].fase.nombre).toEqual("completado");		
+		//usr.iniciarPartida();
+		//expect(juego.partidas[codigo].fase.nombre).toEqual("jugando");		
+		var partida=juego.partidas[codigo];
+		partida.usuarios["tomas"].abandonarPartida();
+		expect(juego.partidas[codigo].fase.nombre).toEqual("inicial");
+		partida.usuarios["isa"].abandonarPartida();
+		partida.usuarios["ana"].abandonarPartida();
+		partida.usuarios["Pepe"].abandonarPartida();
+		expect(partida.numeroJugadores()).toEqual(0);
+		juego.eliminarPartida(codigo);
+		expect(juego.partidas[codigo]).toBe(undefined)
 	})
    });
 })
